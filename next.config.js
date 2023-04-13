55e0860727a81cda2a81c78f41a28b08cedeea3b
castleglint <96310168+castleglint@users.noreply.github.com>
2023-04-13 17:10:50 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ module.exports = withTM({
   swcMinify: false,
   trailingSlash: true,
   env: {
-    HOST_API_KEY: 'https://minimal-assets-api.vercel.app',
+    HOST_API_KEY: 'https://bookshop-nine.vercel.app/',
     // FIREBASE AUTH
     FIREBASE_API_KEY: '',
     FIREBASE_AUTH_DOMAIN: '',
